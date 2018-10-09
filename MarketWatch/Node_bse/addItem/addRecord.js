968aac6deb29060a387d90138e34304044813be3
peeyushn <peeyush.nanhe.ext1@lafargeholcim.com>
2018-10-09 19:30:21 +0530

    add mail fun in add record

--- MarketWatch/Node_bse/addItem/addRecord.js
+++ MarketWatch/Node_bse/addItem/addRecord.js
@@ -29,3 +29,42 @@ module.exports = {
     Stock_InfoTB: Stock_InfoTB
 
 }
+//---------------------------------------
+
+var sendNotificationMail = async function (getUserDeatail, symbol, ltp) {
+    readHTMLFile(__dirname + '/emailWithPDF.html', function (err, html) {
+        var template = handlebars.compile(html);
+        var replacements = {
+            username: getUserDeatail.user_Name
+        };
+        var htmlToSend = template(replacements);
+
+        // rand = Math.floor((Math.random() * 100) + 54);
+        // host = req.get('host');
+        // link = "http://" + req.get('host') + "/api/verifyMail?id=" + rand;
+        mailOptions = {
+            to: getUserDeatail.user_Email,
+            subject: "Your Target Completeed Plese Check your stock",
+            html: htmlToSend
+            // html: "Hello,<br> Please Click on the link to verify your email.<br><a href=" + link + ">Click here to verify</a>"
+        }
+
+        // console.log(link);
+
+        smtpTransport.sendMail(mailOptions, function (error, response) {
+            if (error) {
+                console.log(error);
+                //res.end("error");
+            } else {
+                console.log("Message sent: " + response.message);
+                //res.end("sent");
+            }
+        });
+    });
+
+}
+module.exports = {
+
+    router: router,
+    sendNotificationMail: sendNotificationMail
+};
